80ea063669861908359368583a9d93759e1778b8
pjr <pjr>
2016-03-09 10:39:44 +0100

    Read breakout XML config file

--- Sokoban/CXmlParse.cpp
+++ Sokoban/CXmlParse.cpp
@@ -9,15 +9,63 @@ CXmlParse::CXmlParse() {
 bool CXmlParse::OnLoad(char* File) {
 
 	if (File != NULL) {
-		XMLDocument* doc = new XMLDocument();
-		doc->LoadFile(File);
-		int errorID = doc->ErrorID();
-		delete doc; doc = 0;
-
+		XMLDocument doc = new XMLDocument();
+		XMLError eResult = doc.LoadFile(File);
+		int errorID = doc.ErrorID();
+	
 		printf("Test file '%s' loaded. ErrorID=%d\n", File, errorID);
 		if (errorID) {
 			printf("Failed to load XML configuration\n");
 		}
+
+		XMLNode * pRoot = doc.FirstChild();
+		if (pRoot == nullptr)
+			return XML_ERROR_FILE_READ_ERROR;
+
+		XMLElement * pElement = pRoot->FirstChildElement("Level");
+		if (pElement == nullptr) 
+			return XML_ERROR_PARSING_ELEMENT;
+		
+		//	int
+		int iOutInt;
+		//eResult = pElement->QueryIntText(&iOutInt);
+		eResult = pElement->QueryIntAttribute("RowCount", &iOutInt);
+		XMLCheckResult(eResult);
+
+		//	float
+		pElement = pRoot->FirstChildElement("Level");
+		if (pElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
+
+		float fOutFloat;
+		//eResult = pElement->QueryFloatText(&fOutFloat);
+		eResult = pElement->QueryFloatAttribute("RowCount", &fOutFloat);
+		XMLCheckResult(eResult);
+
+		//	text
+		const char * szAttributeText = nullptr;
+		pElement = pElement->FirstChildElement("Bricks");
+		szAttributeText = pElement->GetText();
+		if (szAttributeText == nullptr) return XML_ERROR_PARSING_ATTRIBUTE;
+		std::string strOutMonth = szAttributeText;
+
+		//	list of elements
+		pElement = pRoot->FirstChildElement("Level")->FirstChildElement("BrickTypes");
+		if (pElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
+
+		XMLElement * pListElement = pElement->FirstChildElement("BrickType");
+		std::vector<int> vecList;
+
+		while (pListElement != nullptr)
+		{
+			int iOutListValue;
+			eResult = pListElement->QueryIntAttribute("HitPoints", &iOutListValue);
+			XMLCheckResult(eResult);
+
+			vecList.push_back(iOutListValue);
+			pListElement = pListElement->NextSiblingElement("BrickType");
+		}
+
+		delete &doc;
 	}
 
 	return true;
--- Sokoban/CXmlParse.h
+++ Sokoban/CXmlParse.h
@@ -4,6 +4,10 @@
 #include <vector>
 #include <tinyxml2.h>
 
+#ifndef XMLCheckResult
+#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
+#endif
+
 using namespace tinyxml2;
 
 class CXmlParse {
